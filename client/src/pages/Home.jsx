e624a6512b6879bf16dca17120099be798c5e0ef
Draacubat <sirppesipuli94@outlook.com>
2023-11-25 12:45:58 +0200

    Edited the Customize It-button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { useSnapshot } from 'valtio'
 
 import state from '../store';
+import { CustomButton } from '../components';
 
 import { 
     headContainerAnimation,
@@ -32,12 +33,19 @@ const Home = () => {
               </h1>
             </motion.div>
 
-            <motion.div>
-              <p>
+            <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
+              <p className='max-w-md font-normal text-gray-600 text-base'>
               Create your unique and exlusive shirt with our brand-new 3D
               customization tool. <strong>Unleash your imagination</strong>{" "} 
               and define your own style. 
               </p>
+
+              <CustomButton 
+                type="filled"
+                title= "Customize It"
+                handLeClick={() => state.intro = false}
+                CustomStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
         </motion.section>

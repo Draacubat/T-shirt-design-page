f4db54c23cd7443e6a28d85b695d0a64c23140c0
Draacubat <sirppesipuli94@outlook.com>
2023-11-26 15:21:40 +0200

    Added the tabs on Customizer-page and you can return back to the main page.

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -35,6 +35,30 @@ const Customizer = () => {
             </div>
 
           </motion.div>
+
+          <motion.div 
+          className="absolute z-10 top-5 right-5"
+          {...fadeAnimation}>
+            <CustomButton 
+              type="filled"
+              title="Go Back"
+              handLeClick={() => state.intro = true}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation('up')}>
+              {FilterTabs.map((tab) =>
+                <Tab
+                  key={tab.name}
+                  tab={tab}
+                  isFilterTab
+                  isActiveTab=""
+                  handLeClick= {() => {}}
+                />)}
+            </motion.div>
         </>
       )}
     </AnimatePresence>

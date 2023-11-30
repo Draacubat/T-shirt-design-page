2421470af323582568bb43136ca9f6013441f969
Draacubat <sirppesipuli94@outlook.com>
2023-11-30 16:47:41 +0200

    Changed the opaxity on the shadows. Still have to be worked on

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,27 +1,42 @@
 import React, { useRef } from 'react'
 import { easing } from 'maath'
-import { useFrame } from '@react-three/fiber'
+import { useFrame, useThree } from '@react-three/fiber'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
+import * as THREE from 'three'; // Import Three.js
 
 const Backdrop = () => {
+  const { scene } = useThree();
+
+  scene.background = new THREE.Color('white');
+
   const shadows = useRef();
+
   return (
     <AccumulativeShadows
       ref={shadows}
       temporal
       frames={60}
-      alphaTest={0.85}
-      scae={10}
+      alphaTest={0.95}
+      scale={10}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
+      opacity={0.65}
     >
       <RandomizedLight
         amount={4} 
         radius={9}
-        intensity={0.55}
+        intensity={0.75}
         ambient={0.25}
         position={[5, 5, -10]}
-      />  
+      />
+
+      <RandomizedLight
+        amount={4} 
+        radius={5}
+        intensity={0.25}
+        ambient={0.15}
+        position={[-5, 5, -9]}
+      />      
     </AccumulativeShadows>
   )
 }
